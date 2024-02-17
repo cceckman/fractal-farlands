c160eb0637cd4b3ff06719c2a9ff0693fa94b05f
Charles Eckman <charles@cceckman.com>
2024-02-17 09:50:19 -0500

    Set up a basic web server (axum)

--- ff-web/src/main.rs
+++ ff-web/src/main.rs
@@ -1,3 +1,16 @@
+use ff_web::root_routes;
+
 fn main() {
-    println!("Hello, world!");
+    let web_rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("could not construct Tokio runtime");
+    let handle = web_rt.handle().clone();
+
+    let server = async {
+        let app = root_routes(handle);
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+        axum::serve(listener, app).await
+    };
+    web_rt.block_on(server).expect("server terminated");
 }

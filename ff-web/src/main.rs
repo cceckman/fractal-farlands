9db09b9226819f903c545b1b67f04acdf8566e8e
Charles Eckman <charles@cceckman.com>
2024-02-18 14:24:55 -0500

    Switch 'log' to 'trace'

--- ff-web/src/main.rs
+++ ff-web/src/main.rs
@@ -1,17 +1,42 @@
+use axum::http::Request;
 use ff_web::root_routes;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt,util::SubscriberInitExt};
 
 fn main() {
-    stderrlog::new().module(module_path!()).init().unwrap();
-
     let web_rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .expect("could not construct Tokio runtime");
     let handle = web_rt.handle().clone();
 
-    let server = async {
-        let app = root_routes(handle);
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    // Tracing config, from the Axum example:
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "ff-web=debug,ff_web=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let trace = TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
+        // Log the path and query string
+        let query = request.uri().path_and_query().map(|v| v.as_str()).unwrap_or("");
+        tracing::info_span!(
+            "http_request",
+            method = ?request.method(),
+            query,
+        )
+    });
+
+    let server = async move {
+        let app = root_routes(handle).layer(trace);
+        const ADDR : &str = "0.0.0.0:3000";
+        let listener = tokio::net::TcpListener::bind(ADDR).await?;
+        tracing::info!("listening at {:?}", ADDR);
         axum::serve(listener, app).await
     };
     web_rt.block_on(server).expect("server terminated");

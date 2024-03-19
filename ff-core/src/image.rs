46ec201e11b946a23addaa036d7a1fcd136d1bec
Stephen Longfield <stephen.longfield@gmail.com>
2024-03-18 22:49:37 -0700

    Use a library for hsv conversion.

--- ff-core/src/image.rs
+++ ff-core/src/image.rs
@@ -1,5 +1,7 @@
 use crate::{Escape, EscapeVector, Size, Zero, ZeroVector};
 
+use hsv;
+
 /// Settings for rendering a fractal into an image.
 #[derive(Default)]
 pub struct Renderer {}
@@ -101,42 +103,11 @@ fn value_to_rgb(min: usize, max: usize, value: usize, escape: f64) -> image::Rgb
         v => v,
     };
     // Smooth Mandelbrot coloring from https://mrob.com/pub/muency/continuousdwell.html
-    let offset: i64 =
-        ((4.0f64.log2().log2() - escape.log2().log2()) * (360.0f64 / (denom as f64))) as i64;
+    let offset: i64 = ((4.0f64.log2().log2() - escape.log2().log2()) * 360.0) as i64; 
     let hue_numerator = (value - min) as i64;
     // H in range [0, 360]
-    let hue = ((hue_numerator * 360) / denom) + offset;
-
-    // Formulas from Wikipedia:
-    //
-    // Chroma:
-    //  C = (1 - |2L - 1|) * S_l
-    // But we're fully saturating L=0.5, S=1, so C = 1.
-
-    //  H' = H / 60deg
-    let hprime = (hue as f64) / 60.0;
-    //  X = C * (1 - |H' mod 2 - 1|)
-    // C == 1, so we can reduce that term out.
-    let x = 1.0 - ((hprime % 2.0) - 1.0).abs();
-
-    //  m = L - C / 2
-    // but with L=0.5 and C = 1, m == 0.
-    // And we want to scale to 0, 255...so:
-    let x = (x * 255.0) as u8;
-    let c = 255;
-    let (r, g, b) = if hprime < 1.0 {
-        (c, x, 0)
-    } else if hprime < 2.0 {
-        (x, c, 0)
-    } else if hprime < 3.0 {
-        (0, c, x)
-    } else if hprime < 4.0 {
-        (0, x, c)
-    } else if hprime < 5.0 {
-        (x, 0, c)
-    } else {
-        (c, 0, x)
-    };
-
+    // https://stackoverflow.com/questions/31210357/is-there-a-modulus-not-remainder-function-operation
+    let hue = (((hue_numerator * 360 + offset) / denom) % 360 + 360)%360;
+    let (r,g,b) = hsv::hsv_to_rgb(hue as f64, 1.0, 1.0);
     image::Rgb([r, g, b])
 }

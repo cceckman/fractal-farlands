3752cbe6f5ee44b98a642d8c4e007cd8e8e0c2fe
Stephen Longfield <stephen.longfield@gmail.com>
2024-03-21 22:24:57 -0700

    Fix select_nth_unstable panic when there's no values.
    Also run `cargo fmt`.

--- ff-core/src/image.rs
+++ ff-core/src/image.rs
@@ -101,11 +101,16 @@ impl NewtonRenderer {
             })
             .collect();
         let len = sort_data.len();
-        let (_, high_iters, _) = sort_data.select_nth_unstable((len as f64 * 0.9) as usize);
+        let high_iters = if len > 3 {
+            let (_, i, _) = sort_data.select_nth_unstable((len as f64 * 0.9) as usize);
+            *i
+        } else {
+            100
+        };
 
         let pixel_values = data.into_iter().map(|v| match v {
             None => image::Rgb([0, 0, 0]),
-            Some(Zero { count, zero }) => newton_to_rgb(max_zero + 1, zero, *high_iters, count),
+            Some(Zero { count, zero }) => newton_to_rgb(max_zero + 1, zero, high_iters, count),
         });
 
         let mut img =

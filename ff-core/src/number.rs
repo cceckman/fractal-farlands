5ee89f192a9e70f98e2515000a1e3dc8ba388894
Charles Eckman <charles@cceckman.com>
2024-03-24 00:48:21 -0400

    Writeup hack to generate header values

--- ff-core/src/number.rs
+++ ff-core/src/number.rs
@@ -72,6 +72,52 @@ impl<const E: usize, const F: usize> FractalNumber for MaskedFloat<E, F> {
     }
 }
 
+fn to_fixed<F>(value: &BigRational) -> Result<F, String>
+where
+    F: fixed::traits::Fixed + std::ops::Neg<Output = F>,
+{
+    let zero: BigInt = 0.into();
+    // To make it easier to reason about fractions & numbers-
+    // Do everything as "positive", then negate.
+    let negative = value.is_negative();
+    let value = if negative { -value } else { value.clone() };
+
+    let whole = value
+        .trunc()
+        .to_integer()
+        .to_i128()
+        .ok_or_else(|| format!("big-rational {} out of range", value.trunc()))?;
+    let part = value.fract();
+
+    // Perform binary long-division on the fractional part;
+    // get one more bit of precision than we have.
+    let mut remainder = part.numer().clone();
+    let mut fraction: i128 = 0;
+    for i in 0..=F::FRAC_NBITS {
+        remainder = remainder << 1;
+        let new_rem = &remainder - part.denom();
+        if new_rem >= zero {
+            remainder = new_rem;
+            fraction |= 1 << (F::FRAC_NBITS - i);
+        }
+    }
+    // We kept an extra bit of precision so we can round.
+    let fraction = if (fraction & 1) == 1 {
+        // Round up:
+        (fraction >> 1) | 1
+    } else {
+        // Round down:
+        fraction >> 1
+    };
+
+    let v = F::from_bits(
+        ((whole << F::FRAC_NBITS) | fraction)
+            .try_into()
+            .map_err(|_| "truncated rational".to_string())?,
+    );
+    Ok(if negative { -v } else { v })
+}
+
 /// Implementation of MandelbrotNumber for fixed-precision formats.
 /// Needs at least 4 bits of integer part to allow "4" + sign.
 macro_rules! impl_fixed {
@@ -88,47 +134,7 @@ macro_rules! impl_fixed {
 
         impl FromRational for $t {
             fn from_bigrational(value: &BigRational) -> Result<Self, String> {
-                let zero: BigInt = 0.into();
-                // To make it easier to reason about fractions & numbers-
-                // Do everything as "positive", then negate.
-                let negative = value.is_negative();
-                let value = if negative { -value } else { value.clone() };
-
-                let whole = value
-                    .trunc()
-                    .to_integer()
-                    .to_i128()
-                    .ok_or_else(|| format!("big-rational {} out of range", value.trunc()))?;
-                let part = value.fract();
-                const FRAC: u32 = <$t>::FRAC_NBITS;
-
-                // Perform binary long-division on the fractional part;
-                // get one more bit of precision than we have.
-                let mut remainder = part.numer().clone();
-                let mut fraction: i128 = 0;
-                for i in 0..=FRAC {
-                    remainder = remainder << 1;
-                    let new_rem = &remainder - part.denom();
-                    if new_rem >= zero {
-                        remainder = new_rem;
-                        fraction |= 1 << (FRAC - i);
-                    }
-                }
-                // We kept an extra bit of precision so we can round.
-                let fraction = if (fraction & 1) == 1 {
-                    // Round up:
-                    (fraction >> 1) | 1
-                } else {
-                    // Round down:
-                    (fraction >> 1)
-                };
-
-                let v = <$t>::from_bits(
-                    ((whole << FRAC) | fraction)
-                        .try_into()
-                        .map_err(|_| "truncated rational".to_string())?,
-                );
-                Ok(if negative { -v } else { v })
+                to_fixed::<$t>(value)
             }
         }
     };
@@ -316,5 +322,27 @@ mod tests {
         const SMALL: P32 = P32::from_f32(1.0 / 16.0);
         let small = BigRational::new(1.into(), 16.into());
         assert_eq!(P32::from_bigrational(&small).unwrap(), SMALL);
+    }
+
+    #[test]
+    fn test_headers() {
+        // Get the various representations for section headers
+        let four_thirds = BigRational::new(4.into(), 3.into());
+        let f = f32::from_bigrational(&four_thirds).unwrap();
+        print!("{:2.11}\n", f);
+
+      //let p4 = P32::from_bigrational(&four_thirds).unwrap();
+      //let p5 = P32::from_bigrational(&five_thirds).unwrap();
+      //print!("{:2.99}\n{:2.99}\n", p4, p5);
+
+        // 2147483647 is the max that LaTeX will take.
+        // 3330078125
+
+        let n = 6;
+        for i in 1..=n {
+            let r = BigRational::new(i.into(), (n + 1).into());
+            let fixie : fixed::types::I23F9 = to_fixed(&r).unwrap();
+            print!("{:2.11}\n", fixie);
+        }
     }
 }
